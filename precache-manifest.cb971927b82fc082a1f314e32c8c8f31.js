cfc5479a884a3f872e31bf1ff1bc74b1b7a1c360
=Bhusallaxman22 <=laxmanbhusal612@gmail.com>
2019-05-03 23:52:54 +0545

    Updates

---
+++ precache-manifest.cb971927b82fc082a1f314e32c8c8f31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0060e545d4293f7a730fde31a261e3fb",
+    "url": "/Shows/index.html"
+  },
+  {
+    "revision": "5f51e46e2f23502c19bc",
+    "url": "/Shows/static/css/2.d9482dc6.chunk.css"
+  },
+  {
+    "revision": "2f2d1e601143dc56d290",
+    "url": "/Shows/static/css/main.c90df7dc.chunk.css"
+  },
+  {
+    "revision": "5f51e46e2f23502c19bc",
+    "url": "/Shows/static/js/2.c4feffe9.chunk.js"
+  },
+  {
+    "revision": "2f2d1e601143dc56d290",
+    "url": "/Shows/static/js/main.7220d5f7.chunk.js"
+  },
+  {
+    "revision": "37e64b7b423ee7927a06",
+    "url": "/Shows/static/js/runtime~main.11ee6706.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Shows/static/media/SEGA.12a3db8b.woff"
+  }
+]);
