04b048ab6e893289786aafa188e2e09e1b1de87b
Laxman Bhusal <laxmanbhusal612@gmail.com>
2019-06-29 16:09:53 +0545

    aaa_bbb

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Rating from './rating'
+
 const Card = ({ name, id, No_Of_Current_Seasons, Status, Plot, imdb, link}) => {
   if (`${id}` % 2 !== 0) {
       return (
@@ -7,7 +8,7 @@ const Card = ({ name, id, No_Of_Current_Seasons, Status, Plot, imdb, link}) => {
     <div className="flip-container">
   <div className="flipper">
     <div className="front">
-    <img src = {`./${id}.jpg`} alt='shows' />
+    <img src = {`./images/${id}.jpg`} alt='shows' />
     <div>
     <h2 className='name navy'>{id}.{name}</h2>
     <p>No. Of Current Seasons: {No_Of_Current_Seasons}</p>
@@ -17,6 +18,7 @@ const Card = ({ name, id, No_Of_Current_Seasons, Status, Plot, imdb, link}) => {
     <div className="back">
     <p className="red">Plot:</p><p className='bg-gray near-black br3'> {Plot}</p>
     <p className="navy">IMDB: {imdb}</p>
+    {/* <IMDB /> */}
     <Rating />
     <a  href={`${link}`} className='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-hot-pink'>Link</a>
     </div>
@@ -30,7 +32,7 @@ const Card = ({ name, id, No_Of_Current_Seasons, Status, Plot, imdb, link}) => {
       <div className="flip-container">
     <div className="flipper">
       <div className="front">
-      <img src = {`${id}.jpg`} alt='shows' />
+      <img src = {`./images/${id}.jpg`} alt='shows' />
       <div>
       <h2 className='name navy'>{id}.{name}</h2>
       <p>No. Of Current Seasons: {No_Of_Current_Seasons}</p>
@@ -40,7 +42,6 @@ const Card = ({ name, id, No_Of_Current_Seasons, Status, Plot, imdb, link}) => {
       <div className="back">
       <p className="red">Plot:</p><p className='bg-gold br3'> {Plot}</p>
       <p className="light-yellow">IMDB: {imdb}</p>
-      {/* <IMDB /> */}
       <Rating />
       <a  href={`${link}`} className='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-hot-pink'>Link</a>
       </div>
